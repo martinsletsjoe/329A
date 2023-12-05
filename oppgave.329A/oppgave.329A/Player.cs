f00450abcea8757fc54507991b60b013341b8b0f
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2023-12-05 15:15:16 +0100

    329a

---
+++ oppgave.329A/oppgave.329A/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace oppgave._329A;
+
+public class Player
+{
+    public string Name { get; }
+    public int Points { get; private set; }
+
+
+    public Player(string name, int points)
+    {
+        Name = name;
+        Points = points;
+    }
+    public void Play(Player player)
+    {
+        Random random = new Random();
+        int num = random.Next(2);
+
+        if (num % 2 == 0)
+        {
+            player.Points++;
+        }
+        else
+        {
+            player.Points--;
+        }
+    }
+
+    public void ShowNameAndPoints()
+    {
+        Console.WriteLine($"{Name} har {Points} poeng");
+    }
+}
